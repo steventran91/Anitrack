97ce5d4f7804fc72b6ac76591f7fcdc23a223993
Steven Tran <sdtran91@gmail.com>
2026-07-15 13:28:28 -0700

    set up anime search result schema

---
+++ backend/app/schemas/anime.py
@@ -0,0 +1,12 @@
+from pydantic import BaseModel, HttpUrl
+
+class AnimeSearchResult(BaseModel):
+    id: int
+    title_english: str | None = None
+    title_native: str | None = None 
+    image: HttpUrl
+    episodes: int | None = None 
+    status: str 
+    genres: list[str]
+    average_score: int | None = None 
+
